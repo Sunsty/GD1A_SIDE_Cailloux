522913569257a0b73e6c5fe8e78974daa114babd
Sunsty <cailloux.baptiste@gmail.com>
2024-01-31 12:22:46 +0100

    31/01/2024

---
+++ Assets/Script/Enemy_Patrol.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Enemy_Patrol : MonoBehaviour
+{
+    public float speed;
+    public Transform[] waypoints;
+
+    public SpriteRenderer graphics;
+
+    private Transform target;
+    private int destPoint;
+    void Start()
+    {
+        target = waypoints[0];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if(Vector2.Distance(transform.position, target.position) < 0.3f)
+        {
+            destPoint = (destPoint + 1) % waypoints.Length;
+            target = waypoints[destPoint];
+            graphics.flipX = !graphics.flipX;
+        }
+    }
+}
